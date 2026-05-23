150934eb5e1dc742a914db4400274f8ca45e0638
Guilherme Brandao <brandao.dev0@gmail.com>
2026-05-23 10:44:59 -0300

    Creat continue;

---
+++ src/exercicios/Aula06ExercicioDeEstruturaDeRepeticao03.java
@@ -0,0 +1,20 @@
+package exercicios;
+
+public class Aula06ExercicioDeEstruturaDeRepeticao03 {
+    public static void main(String[] args) {
+
+        // Dado o valor de um carro, descubra em quantas vezes ele pode ser parcelado
+        // Condição é que o ValorParcela >= 1000
+        // O ValorCarro = 48000
+
+        double ValorTotal = 30000;
+
+        for (int Parcela = (int) 30000; Parcela >= 1; Parcela--) {
+            double ValorParcela = ValorTotal / Parcela;
+            if (ValorParcela < 1000){
+                continue;
+            }
+            System.out.println("Parcela " + Parcela + " R$ " + ValorParcela);
+        }
+    }
+}
