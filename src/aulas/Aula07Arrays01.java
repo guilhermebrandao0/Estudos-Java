77a2bbf90f0ef82832076c45b028db3e099f1c5c
Guilherme Brandao <brandao.dev0@gmail.com>
2026-05-23 16:21:09 -0300

    array with for

--- src/aulas/Aula07Arrays01.java
+++ src/aulas/Aula07Arrays01.java
@@ -13,7 +13,7 @@ public class Aula07Arrays01 {
         // Em um Array a gente sempre começa na posição 0, por mas que tenha 3 espaços,
         // contamos a partir do 0 ai vai "0,1,2." = "1,2,3 espaços"
 
-        System.out.println("Idade de Sra.Ana é "+idades[0]);
+        System.out.println("Idade da Sra.Ana é "+idades[0]);
         System.out.println("Idade de Joãozinho é "+idades[1]);
         System.out.println("Idade do Sr.Pedro é "+idades[2]);
     }
--- src/aulas/Aula07Arrays02.java
+++ src/aulas/Aula07Arrays02.java
@@ -1,4 +1,34 @@
 package aulas;
 
 public class Aula07Arrays02 {
+    public static void main(String[] args) {
+
+        // byte, short, int, long, float, long e double = 0
+        // char = '\u0000' ' '
+        // boolean = false
+        // String = null
+
+        // aqui vamos utilizar nomes e idades
+
+        String [] nomes = new String[3];
+        nomes[0] = "Ana";
+        nomes[1] = "Joãozinho";
+        nomes[2] = "Mário";
+        int [] idades = new int[3];
+        idades[0] = 21;
+        idades[1] = 11;
+        idades[2] = 33;
+
+        System.out.println("Meu nome é "+nomes[0]+" e eu tenho "+idades[0]+" anos.");
+        System.out.println("Meu nome é "+nomes[1]+" e eu tenho "+idades[1]+" anos.");
+        System.out.println("Meu nome é "+nomes[2]+" e eu tenho "+idades[2]+" anos.");
+
+        System.out.println( );
+
+        // Outra forma de fazer arrays
+
+        for (int i = 0; i < nomes.length; i++){
+            System.out.println("Meu nome é "+nomes[i]+" e eu tenho "+idades[i]+" anos.");
+        }
+    }
 }
