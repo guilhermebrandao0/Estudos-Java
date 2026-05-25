622d3198c5698de91d19a6ee83e3f441690de7e0
Guilherme Brandao <brandao.dev0@gmail.com>
2026-05-24 21:52:48 -0300

    calculadora em produção

---
+++ src/aulas/javacore/Bintroducaometodos/test/CalculadoraTest01.java
@@ -0,0 +1,16 @@
+package aulas.javacore.Bintroducaometodos.test;
+
+import aulas.javacore.Bintroducaometodos.dominio.Calculadora;
+
+public class CalculadoraTest01 {
+    public static void main(String[] args) {
+
+        Calculadora calculadora = new Calculadora();
+
+        calculadora.somaDoisNumeros();
+        System.out.println( );
+        calculadora.subtraiDoisNumeros();
+        System.out.println("Finalizando calculadoraTest01");
+
+    }
+}
