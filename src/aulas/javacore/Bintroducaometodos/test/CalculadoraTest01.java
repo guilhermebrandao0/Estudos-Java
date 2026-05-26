2218cd0f4d2ad9d0a3ecf24ffe04d11020466f38
Guilherme Brandao <brandao.dev0@gmail.com>
2026-05-26 03:27:05 -0300

    mult

--- src/aulas/javacore/Bintroducaometodos/test/CalculadoraTest01.java
+++ src/aulas/javacore/Bintroducaometodos/test/CalculadoraTest01.java
@@ -6,10 +6,18 @@ public class CalculadoraTest01 {
     public static void main(String[] args) {
 
         Calculadora calculadora = new Calculadora();
-
         calculadora.somaDoisNumeros();
+
         System.out.println( );
+
         calculadora.subtraiDoisNumeros();
+
+        System.out.println( );
+
+        calculadora.multiplicaDoisNumeros(10, 20);
+
+        System.out.println( );
+
         System.out.println("Finalizando calculadoraTest01");
 
     }
