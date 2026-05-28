9fe762e75349ac3fede9f3555430c6fc7c672eca
Guilherme Brandao <brandao.dev0@gmail.com>
2026-05-28 16:31:06 -0300

    Update Calculadora.java

--- src/aulas/javacore/Bintroducaometodos/dominio/Calculadora.java
+++ src/aulas/javacore/Bintroducaometodos/dominio/Calculadora.java
@@ -1,15 +1,19 @@
 package aulas.javacore.Bintroducaometodos.dominio;
 
 public class Calculadora {
+
     public void somaDoisNumeros(int num1, int num2){
         System.out.println(num1 + num2);
     }
+
     public void subtraiDoisNumeros(int num3, int num4){
         System.out.println(num3 - num4);
     }
+
     public void multiplicaDoisNumeros(int num5, int num6){
         System.out.println(num5 * num6);
     }
+
     public  void dividirDoisNumeros(int num7, int num8){
         System.out.println(num7 / num8);
     }
