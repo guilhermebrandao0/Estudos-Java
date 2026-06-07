5067df579c8de4a79ab3e70008fce78d9e19ea01
Guilherme Brandao <brandao.dev0@gmail.com>
2026-06-07 15:56:30 -0300

    return

---
+++ src/aulas/javacore/Bintroducaometodos/test/CalculadoraTest02.java
@@ -0,0 +1,15 @@
+package aulas.javacore.Bintroducaometodos.test;
+
+import aulas.javacore.Bintroducaometodos.dominio.Calculadora;
+
+public class CalculadoraTest02 {
+    public static void main(String[] args) {
+
+        // continuação dos execicios de calculadora
+
+        Calculadora calculadora2 = new Calculadora();
+
+       double result = calculadora2.dividirDoisNumerosD(20, 2);
+        System.out.println(result);
+    }
+}
