cc8b5383f7a951ee8476f9ce5d1c5829eddbd727
Guilherme Brandao <brandao.dev0@gmail.com>
2026-06-08 23:07:37 -0300

    novo projeto

---
+++ src/projetos/SistemaDeEmprestimoBancario.java
@@ -0,0 +1,52 @@
+package projetos;
+
+public class SistemaDeEmprestimoBancario {
+    public static void main(String[] args) {
+
+        System.out.println( );
+
+        // Um banco analisa se uma pessoa pode pegar um empréstimo.
+
+        //
+
+        //Regras
+        //Se o salário for menor que R$ 2.000 → Empréstimo negado.
+        //Se o salário for entre R$ 2.000 e R$ 5.000 → Empréstimo máximo de R$ 10.000.
+        //Se o salário for maior ou igual a R$ 5.000 → Empréstimo máximo de R$ 50.000.
+        //Pessoas
+
+        //
+
+        //Crie pelo menos 4 pessoas:
+        //Guilherme
+        //Beatryz
+        //Stefan
+        //Damon
+        // Cada uma com um salário diferente.
+
+        //
+
+        //O programa deve mostrar
+        //
+        //Exemplo:
+        //
+        //Nome: Guilherme
+        //Salário: R$ 1800.00
+        //Status: Empréstimo negado
+        //
+        //ou
+        //
+        //Nome: Stefan
+        //Salário: R$ 3500.00
+        //Status: Empréstimo aprovado
+        //Valor máximo disponível: R$ 10000.00
+        //
+        //ou
+        //
+        //Nome: Beatryz
+        //Salário: R$ 7000.00
+        //Status: Empréstimo aprovado
+        //Valor máximo disponível: R$ 50000.00
+
+    }
+}
