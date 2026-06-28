71ddd28237e8ae901a991f0e6bcfbffdf843e878
Guilherme Brandao <brandao.dev0@gmail.com>
2026-06-28 14:35:27 -0300

    aprendendo metodos

--- src/aulas/javacore/Bintroducaometodos/dominio/Calculadora.java
+++ src/aulas/javacore/Bintroducaometodos/dominio/Calculadora.java
@@ -24,4 +24,14 @@ public class Calculadora {
         return num9/num10;
     }
 
+    //
+
+    public void alteraDoisNumeros(int num1, int num2){
+        num1 = 99;
+        num2 = 33;
+        System.out.println("Dentro do alteraDoisNumeros");
+        System.out.println("num1 " + num1);
+        System.out.println("num2 " + num2);
+    }
+
     }
